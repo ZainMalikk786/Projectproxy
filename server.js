5b6606c28fc38f13619ddeb988fda3edb0a24301
Zain Malik <zainalimalik7676@gmail.com>
2025-08-19 11:01:36 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
+const app = express();
+
+// Signal ka official server URL ya jis server ka proxy banana hai
+const SIGNAL_SERVER_URL = 'https://signal.org';
+
+app.use('/', createProxyMiddleware({
+  target: SIGNAL_SERVER_URL,
+  changeOrigin: true,
+  logLevel: 'debug'
+}));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Signal proxy running on port ${PORT}`);
+});
